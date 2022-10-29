d3eeda8281c765501d1735009db4864695271bb6
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-10-29 17:27:20 +0700

    fix error latlngbound

--- src/components/ControllingGroup.tsx
+++ src/components/ControllingGroup.tsx
@@ -3,10 +3,12 @@ import { latLng, LatLng, LocationEvent } from "leaflet";
 import L from "leaflet";
 import { LayersControl, useMapEvents } from "react-leaflet";
 
+const center: [number, number] = [13.955392, 108.676758];
+
 const ControllingGroup: React.FC = () => {
   const map = useMapEvents({
     overlayadd(e) {
-      const bounds = new L.LatLngBounds();
+      const bounds = new L.LatLngBounds(center, center);
 
       map.eachLayer(function (layer) {
         if (layer instanceof L.FeatureGroup) {
