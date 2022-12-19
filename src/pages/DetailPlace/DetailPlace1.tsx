04b8202fa0f04ce63d81b02094f860bc383af15e
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-12-19 21:41:29 +0700

    modify ui detail-place

---
+++ src/pages/DetailPlace/DetailPlace1.tsx
@@ -0,0 +1,573 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./DetailPlace.scss";
+
+import { Header, Footer, Comments } from "../../components";
+
+import Media from "./Media";
+import Summary from "./Summary";
+import Rating from "./Rating";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import ReactReadMoreReadLess from "react-read-more-read-less";
+
+import {
+  Typography,
+  Row,
+  Col,
+  Space,
+  Button,
+  Divider,
+  Avatar,
+  Image,
+  Rate,
+} from "antd";
+
+import Icon, {
+  CommentOutlined,
+  PlayCircleOutlined,
+  StarFilled,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+
+import ggMapIcon from "../../assets/img/Google_Maps.svg.png";
+import { url } from "inspector";
+import TextArea from "antd/lib/input/TextArea";
+import apiPlaces from "../../apis/apiPlaces";
+
+const { Title, Text } = Typography;
+
+interface urlImage {
+  url: string;
+}
+interface place {
+  name: string;
+  place_type: {
+    name: string;
+  };
+  place_img: urlImage[];
+}
+
+const DetailPlace: React.FC = () => {
+  const { id } = useParams();
+
+  const [place, setPlace] = useState<place>({
+    name: "",
+    place_type: { name: "" },
+    place_img: [
+      {
+        url: "https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/08/brazil-single-2-2.jpg",
+      },
+      {
+        url: "https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/08/brazil-single-2-2.jpg",
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const getDetailPlace = async () => {
+      apiPlaces
+        .getPlace(id)
+        .then((res) => {
+          setPlace(res.data);
+        })
+        .catch((err) => console.log(err));
+    };
+    getDetailPlace();
+  }, []);
+
+  // console.log(place.place_img);
+
+  return (
+    <div style={{ minHeight: "200rem" }} className="primary-font">
+      <Header />
+
+      <Row style={{}} className="mb-5">
+        <Row
+          style={{ marginTop: "5rem" }}
+          className="bg-img rounded"
+          justify="center"
+        >
+          <Col span={12} className="position-relative">
+            <div
+              style={{
+                width: "70%",
+                backgroundColor: "#292D33",
+                padding: "2rem",
+                zIndex: 1,
+                top: 80,
+              }}
+              className="h-50 ms-5 p-5 position-absolute"
+            >
+              <Text style={{ fontSize: "1.6rem" }} className="text-white">
+                Loại địa điểm
+              </Text>
+              <Title
+                style={{ fontSize: "2.8rem" }}
+                className="text-white fw-light m-0 mt-4"
+              >
+                Tên địa điểm
+              </Title>
+            </div>
+            <div
+              style={{
+                backgroundColor: "#F86449",
+                width: "14rem",
+                height: "90%",
+                top: 40,
+                left: 410,
+                zIndex: 0,
+              }}
+              className="position-absolute"
+            ></div>
+          </Col>
+          <Col className="position-relative" span={12}>
+            <div
+              style={{
+                width: "85%",
+                bottom: 100,
+              }}
+              className="bg-white position-absolute shadow p-5"
+            >
+              <Space direction="vertical" size={25}>
+                <Title className="fw-light" level={2}>
+                  Check out all of these physical benefits of hiking
+                </Title>
+                <Text className="fs-6">
+                  Massa ultricies mi quis hendrerit. Ac ut consequat semper
+                  viverra nam. Libero id faucibus nisl tincidunt eget nullam non
+                  nisi est. Arcu odio ut sem nulla. Amet tellus cras adipiscing
+                  enim. Et magnis dis parturient montes. Imperdiet sed euismod
+                  nisi porta lorem mollis.
+                </Text>
+                <Button
+                  style={{ width: "10rem", height: "2.5rem" }}
+                  className=""
+                >
+                  LEARN MORE
+                </Button>
+              </Space>
+            </div>
+          </Col>
+        </Row>
+
+        <Row
+          style={{ marginTop: "5rem", padding: "0 6rem" }}
+          justify="center"
+          align="middle"
+        >
+          <Col span={6}>
+            <Space style={{}} className="" direction="vertical" size={15}>
+              <Text style={{ fontSize: "2rem" }} className="" strong>
+                Tên địa điểm
+              </Text>
+              <Text
+                style={{ fontSize: "1.2rem", color: "#FF7424" }}
+                className="mt-0"
+                strong
+              >
+                Loại địa điểm
+              </Text>
+
+              <Text style={{ fontSize: "0.9rem" }}>
+                Lorem ipsum dolor sit amet adipisicing elit. Inventore id iure
+                maxime repellendus a rem quisquam eum nisi et aliquam deserunt
+                quam architecto ducimus magnam, doloribus sint, consequatur,
+                nemo ipsam!
+              </Text>
+
+              <Button
+                style={{ width: "15rem", height: "3rem" }}
+                className="bg-black mt-2"
+              >
+                <Text className="text-white fs-6">Check in</Text>
+                <i
+                  style={{ width: "2rem" }}
+                  className="fa-solid fa-arrow-right text-white "
+                ></i>
+              </Button>
+            </Space>
+          </Col>
+
+          <Col
+            style={{
+              height: "60%",
+              width: "9rem",
+            }}
+            className="d-flex justify-content-center"
+            span={6}
+          >
+            <Summary />
+          </Col>
+
+          <Col className="position-relative" span={10}>
+            <img
+              width={480}
+              className="video-detail"
+              height={500}
+              alt=""
+              src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/08/brazil-single-2-2.jpg"
+            />
+            <PlayCircleOutlined
+              style={{ fontSize: "4rem", top: "43%", right: "43%" }}
+              className="position-absolute text-white"
+            />
+          </Col>
+        </Row>
+
+        <Image.PreviewGroup>
+          <Row
+            style={{
+              textAlign: "center",
+              marginTop: "5rem",
+              width: "100%",
+              height: "700px",
+              padding: "0 0 0 5rem",
+            }}
+            justify="center"
+            align="top"
+            gutter={20}
+          >
+            <Col className="h-100" span={9}>
+              <Image
+                style={{}}
+                className="w-100 vh-100"
+                src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/08/brazil-single-2-2.jpg"
+                // src={images[0].url}
+              />
+            </Col>
+
+            <Col className="h-100" span={6}>
+              <Space className="vh-100" direction="vertical" size={20}>
+                <Image
+                  // style={{ width: "100%", height: "40%" }}
+                  className="w-100"
+                  src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/09/blog-post-h6-img2.jpg"
+                />
+                <Image
+                  // style={{ width: "100%", height: "40%" }}
+                  className="w-100"
+                  src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/09/blog-post-h6-img2.jpg"
+                />
+              </Space>
+            </Col>
+
+            <Col className="h-100" span={5}>
+              <Space className="vh-100" direction="vertical" size={15}>
+                {[0, 1, 2, 3].map((item) => (
+                  <Image
+                    style={{ width: "15rem", height: "7rem" }}
+                    className=""
+                    src="https://prod-virtuoso.dotcmscloud.com/dA/188da7ea-f44f-4b9c-92f9-6a65064021c1/previewImage/PowerfulReasons_hero.jpg"
+                  />
+                ))}
+              </Space>
+            </Col>
+
+            <Col className="h-100" span={2}>
+              <Space className="vh-100" direction="vertical" size={15}>
+                {[0, 1, 2, 3].map((item) => (
+                  <Image
+                    style={{ width: "6rem", height: "6rem" }}
+                    className=""
+                    src="https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA"
+                  />
+                ))}
+              </Space>
+            </Col>
+          </Row>
+        </Image.PreviewGroup>
+
+        <div
+          style={{
+            backgroundColor: "#F7FAFB",
+            margin: "3rem 4rem",
+            padding: "2rem 0rem",
+          }}
+          className="w-100 "
+        >
+          <Space direction="vertical">
+            <Title className="m-0">Bình luận và</Title>
+            <Title style={{ color: "#FF7424" }}>Đánh giá</Title>
+          </Space>
+
+          <Row className="m-0" justify="center" align="middle" gutter={100}>
+            <Rating />
+          </Row>
+
+          <Divider style={{ borderTop: "#FF7424" }} className="mt-5 mb-5" plain>
+            <Text style={{ color: "#FF7424" }} className="fs-5" strong>
+              Tất cả các bài đánh giá
+            </Text>
+          </Divider>
+
+          <Row className="w-100" justify="center">
+            <Comments />
+
+            <Row
+              style={{ width: "75%", marginBottom: "5rem", marginLeft: "9rem" }}
+              className=""
+              gutter={20}
+            >
+              <Col span={1}>
+                <EllipsisOutlined
+                  style={{ marginTop: "3.5rem" }}
+                  className="fs-5"
+                />
+              </Col>
+
+              <Col className="d-flex justify-content-center" span={3}>
+                <Space className="" direction="vertical">
+                  <Button
+                    style={{ width: "5.5rem", marginTop: "3.5rem" }}
+                    type="primary"
+                    size="small"
+                    shape="round"
+                    ghost
+                  >
+                    Like
+                  </Button>
+                  <Button
+                    style={{ width: "5.5rem" }}
+                    type="primary"
+                    size="small"
+                    shape="round"
+                    ghost
+                    danger
+                  >
+                    Dislike
+                  </Button>
+
+                  <Button
+                    style={{ width: "5.5rem" }}
+                    className="mt-1 d-flex align-items-center justify-content-center"
+                    type="text"
+                    size="large"
+                    icon={<CommentOutlined className="fs-4 text-secondary" />}
+                  >
+                    <Text
+                      style={{ fontSize: "0.9rem" }}
+                      className="text-secondary"
+                    >
+                      Trả lời
+                    </Text>
+                  </Button>
+                </Space>
+              </Col>
+
+              <Col span={13}>
+                <Row className="mb-1" justify="end">
+                  <Space size={3}>
+                    <StarFilled
+                      style={{ color: "#292D33", fontSize: "1.2rem" }}
+                    />
+                    <StarFilled
+                      style={{ color: "#292D33", fontSize: "1.2rem" }}
+                    />
+                    <StarFilled
+                      style={{ color: "#292D33", fontSize: "1.2rem" }}
+                    />
+                    <StarFilled
+                      style={{ color: "#E0E0E3", fontSize: "1.2rem" }}
+                    />
+                    <StarFilled
+                      style={{ color: "#E0E0E3", fontSize: "1.2rem" }}
+                    />
+                  </Space>
+                </Row>
+
+                <Row className="mb-1" justify="end">
+                  <Text strong>Highly Recommended Coffee</Text>
+                </Row>
+
+                <Row className="mb-1 text-secondary text-end" justify="end">
+                  <ReactReadMoreReadLess
+                    charLimit={190}
+                    readMoreText={
+                      <Text style={{ color: "#5449A3" }} strong>
+                        Xem thêm
+                      </Text>
+                    }
+                    readLessText={
+                      <Text style={{ color: "#5449A3" }} strong>
+                        Thu gọn
+                      </Text>
+                    }
+                  >
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Cumque, debitis corrupti. Reprehenderit ullam modi excepturi
+                    eaque dolorum. Dolorum sint asperiores ratione repellat
+                    fugit quam dignissimos quibusdam. Ut, saepe? Dicta,
+                    laudantium.
+                  </ReactReadMoreReadLess>
+                </Row>
+
+                <Image.PreviewGroup>
+                  <Space className="d-flex justify-content-end">
+                    {[0, 1, 2].map((item) => (
+                      <Image
+                        style={{ marginRight: "1rem" }}
+                        width={120}
+                        height={120}
+                        src="https://image.thanhnien.vn/w1024/Uploaded/2022/kbfluaa/2021_11_02/qn-mua-may-7180.jpeg"
+                      />
+                    ))}
+                  </Space>
+                </Image.PreviewGroup>
+
+                {/* trả lời */}
+                <div>
+                  <Row
+                    style={{}}
+                    className="mt-3"
+                    justify="end"
+                    align="top"
+                    gutter={10}
+                  >
+                    <Col span={21}>
+                      <Row
+                        className="mb-2"
+                        gutter={15}
+                        justify="end"
+                        align="middle"
+                      >
+                        <Col>
+                          <Space direction="vertical" size={0}>
+                            <Text style={{ fontSize: "0.8rem" }} strong>
+                              Tên người dùng
+                            </Text>
+                            <Text
+                              style={{ fontSize: "0.8rem" }}
+                              className="text-secondary m-0"
+                            >
+                              Ngày đăng
+                            </Text>
+                          </Space>
+                        </Col>
+                        <Col>
+                          <Avatar
+                            size={40}
+                            src="https://wegotthiscovered.com/wp-content/uploads/2022/08/Vegeta-1200x900.jpeg"
+                          />
+                        </Col>
+                      </Row>
+
+                      <Row
+                        style={{ fontSize: "0.8rem" }}
+                        className="mb-2 text-secondary text-end"
+                        justify="end"
+                      >
+                        <ReactReadMoreReadLess
+                          charLimit={150}
+                          readMoreText={
+                            <Text style={{ color: "#5449A3" }} strong>
+                              Xem thêm
+                            </Text>
+                          }
+                          readLessText={
+                            <Text style={{ color: "#5449A3" }} strong>
+                              Thu gọn
+                            </Text>
+                          }
+                        >
+                          Lorem ipsum dolor, sit amet consectetur adipisicing
+                          elit. Cumque, debitis corrupti. Reprehenderit ullam
+                          modi excepturi eaque dolorum. Dolorum sint asperiores
+                          ratione repellat fugit quam dignissimos quibusdam. Ut,
+                          saepe? Dicta, laudantium.
+                        </ReactReadMoreReadLess>
+                      </Row>
+
+                      <Row className="" justify="end">
+                        <Space>
+                          <Button
+                            style={{ width: "5rem" }}
+                            type="primary"
+                            size="small"
+                            shape="round"
+                            ghost
+                          >
+                            Like
+                          </Button>
+                          <Button
+                            style={{ width: "5rem" }}
+                            type="primary"
+                            size="small"
+                            shape="round"
+                            ghost
+                            danger
+                          >
+                            Dislike
+                          </Button>
+                        </Space>
+                      </Row>
+                    </Col>
+
+                    <Col span={3}>
+                      <Text
+                        style={{
+                          color: "#FD665E",
+                          fontSize: "5rem",
+                          lineHeight: "5rem",
+                        }}
+                      >
+                        ”
+                      </Text>
+                    </Col>
+                  </Row>
+                </div>
+                {/* end trả lời */}
+              </Col>
+
+              <Col span={6}>
+                <Row gutter={10} align="middle">
+                  <Col>
+                    <Space direction="vertical" size={0} align="end">
+                      <Text strong>Tên người dùng</Text>
+                      <Text className="text-secondary">Ngày đăng</Text>
+                    </Space>
+                  </Col>
+                  <Col>
+                    <Avatar
+                      size={50}
+                      src="https://wegotthiscovered.com/wp-content/uploads/2022/08/Vegeta-1200x900.jpeg"
+                    />
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Row>
+        </div>
+
+        <Row className="w-100 mt-5" justify="center" gutter={25}>
+          <Button
+            style={{
+              width: "10rem",
+              height: "3rem",
+              backgroundColor: "#FF7424",
+            }}
+            className="border-0"
+            type="primary"
+          >
+            {" "}
+            <Text className="text-white fs-6" strong>
+              Load more
+            </Text>
+          </Button>
+        </Row>
+      </Row>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default DetailPlace;
--- src/pages/DetailPlace/Media.tsx
+++ src/pages/DetailPlace/Media.tsx
@@ -10,6 +10,13 @@ interface IProps {
 }
 
 const Media: React.FC<IProps> = ({ images }) => {
+  const arrImg = images.map((item) => item.url);
+  const arrImgLen = arrImg.length;
+  for (let i = 0; i < 11 - arrImgLen; i++) {
+    arrImg.push("");
+  }
+
+  console.log(arrImg);
   return (
     <div>
       <Image.PreviewGroup>
@@ -21,22 +28,26 @@ const Media: React.FC<IProps> = ({ images }) => {
             height: "700px",
             padding: "0 0 0 5rem",
           }}
-          justify="start"
+          justify="center"
           align="top"
-          gutter={25}
+          gutter={20}
         >
           <Col className="h-100" span={9}>
-            <Image
+            {/* <Image
               style={{}}
               className="w-100 vh-100"
               src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/08/brazil-single-2-2.jpg"
-              // src={images[0].url}
-            />
+            /> */}
+            {arrImg[0] !== "" ? (
+              <Image style={{}} className="w-100 vh-100" src={arrImg[0]} />
+            ) : (
+              <></>
+            )}
           </Col>
 
           <Col className="h-100" span={6}>
             <Space className="vh-100" direction="vertical" size={20}>
-              <Image
+              {/* <Image
                 // style={{ width: "100%", height: "40%" }}
                 className="w-100"
                 src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/09/blog-post-h6-img2.jpg"
@@ -45,31 +56,49 @@ const Media: React.FC<IProps> = ({ images }) => {
                 // style={{ width: "100%", height: "40%" }}
                 className="w-100"
                 src="https://backpacktraveler.qodeinteractive.com/wp-content/uploads/2018/09/blog-post-h6-img2.jpg"
-              />
+              /> */}
+              {arrImg
+                .slice(1, 3)
+                .filter((item) => item !== "")
+                .map((item) => (
+                  <Image className="w-100" src={item} />
+                ))}
             </Space>
           </Col>
 
-          <Col className="h-100" span={4}>
+          <Col className="h-100" span={5}>
             <Space className="vh-100" direction="vertical" size={15}>
-              {[0, 1, 2, 3].map((item) => (
+              {/* {[0, 1, 2, 3].map((item) => (
                 <Image
                   style={{ width: "15rem", height: "7rem" }}
                   className=""
                   src="https://prod-virtuoso.dotcmscloud.com/dA/188da7ea-f44f-4b9c-92f9-6a65064021c1/previewImage/PowerfulReasons_hero.jpg"
                 />
-              ))}
+              ))} */}
+              {arrImg
+                .slice(3, 8)
+                .filter((item) => item !== "")
+                .map((item, index) => (
+                  <Image className="w-100" src={item} />
+                ))}
             </Space>
           </Col>
 
-          <Col className="h-100" span={3}>
+          <Col className="h-100" span={2}>
             <Space className="vh-100" direction="vertical" size={15}>
-              {[0, 1, 2, 3].map((item) => (
+              {/* {[0, 1, 2, 3].map((item) => (
                 <Image
                   style={{ width: "6rem", height: "6rem" }}
                   className=""
                   src="https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA"
                 />
-              ))}
+              ))} */}
+              {arrImg
+                .slice(8, 12)
+                .filter((item) => item !== "")
+                .map((item) => (
+                  <Image className="w-100" src={item} />
+                ))}
             </Space>
           </Col>
         </Row>
