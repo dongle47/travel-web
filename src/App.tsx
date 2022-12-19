80e107398ad69ff3ee9c0ccd2f47ebdddf320814
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-12-19 23:33:27 +0700

    update antd, fix header

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import "./assets/style/App.scss";
 // import './assets/style/_custome.scss'
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import "antd/dist/antd.css";
-
 import AppRoutes from "./router/AppRoutes";
 import { ToastContainer } from "react-toastify";
 
