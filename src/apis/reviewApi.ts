8e9295c3ed3fad4f9d211a8c8789753e1dc252ed
Dong Le <61934131+dongle47@users.noreply.github.com>
2023-02-12 00:34:54 +0700

    model and api list review

--- src/apis/reviewApi.ts
+++ src/apis/reviewApi.ts
@@ -1,14 +1,20 @@
 import { axiosClient } from './axiosClient';
 import { CreateReviewParams } from './../models/review';
+import { PaginateParams } from '../models/common';
 const reviewApi = {
     postReview(params: CreateReviewParams) : Promise<any>{
         const url = '/place-service/review/create'
         return axiosClient.post(url, params)
     },
 
-    getReviews(placeId:string){
-        const url = `/review/list/${placeId}`
-        return axiosClient.get(url)
+    getReviews(placeId:string, paginate: PaginateParams){
+        const url = `/place-service/review/list/${placeId}`
+        return axiosClient.get(url, {
+            params: {
+                page: paginate.page,
+                limit: paginate.limit
+            }
+        })
     }
 }
 
